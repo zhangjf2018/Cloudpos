5b7a143088848fab158d6c9f04733628be79902c
ABNER <414512194@qq.com>
2018-05-16 20:22:27 +0800

    增加我的订单详情

--- app/src/main/java/com/ymt/cloudpos/view/SettingActivity.java
+++ app/src/main/java/com/ymt/cloudpos/view/SettingActivity.java
@@ -1,21 +1,40 @@
 package com.ymt.cloudpos.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.ymt.cloudpos.R;
 import com.ymt.cloudpos.core.BaseActivity;
 
-public class SettingActivity extends BaseActivity {
+public class SettingActivity extends BaseActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setToolBarTitle("设置");
+        findViewById(R.id.ll_logOut).setOnClickListener(this);
+        findViewById(R.id.ll_modifyPasswd).setOnClickListener(this);
     }
 
     @Override
     protected int getLayoutId() {
         return R.layout.activity_setting;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.ll_modifyPasswd:
+                Intent intent = new Intent();
+                intent.setClass(SettingActivity.this, MineModifyPasswordActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.ll_logOut:
+                break;
+            default:
+                break;
+        }
     }
 }
